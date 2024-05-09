72df91404b925da90412ea413dbfa4639f3370e6
saadetsimsek <saadetsimsek13@gmail.com>
2024-05-09 16:24:12 +0300

    Navigation bar mechanics as well as the sections titles for each section

--- Netflix/Controlllers/HomeViewController.swift
+++ Netflix/Controlllers/HomeViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    let secrionTitles: [String] = ["Trending Movies", "Popular", "Trending TV", "Upcoming Movies", "Top Rating"]
 
     private let homeFeedTableView: UITableView = {
         let table = UITableView(frame: .zero, style: .grouped)
@@ -24,6 +26,8 @@ class HomeViewController: UIViewController {
         homeFeedTableView.delegate = self
         homeFeedTableView.dataSource = self
         
+        configureNavigationBar()
+        
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0,
                                               y: 0,
                                               width: view.bounds.width,
@@ -35,13 +39,33 @@ class HomeViewController: UIViewController {
         super.viewDidLayoutSubviews()
         homeFeedTableView.frame = view.frame
     }
+    
+    private func configureNavigationBar(){
+        var image = UIImage(named: "netflixLogo")
+        image = image?.withRenderingMode(.alwaysOriginal) //resmin değişmemesi için
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image,
+                                                           style: .done,
+                                                           target: self,
+                                                           action: nil)
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(image: UIImage(systemName: "person"),
+                            style: .done,
+                            target: self,
+                            action: nil),
+            UIBarButtonItem(image: UIImage(systemName: "play.rectangle"),
+                            style: .done,
+                            target: self,
+                            action: nil)
+        ]
+        navigationController?.navigationBar.tintColor = .systemRed
+    }
 
 
 }
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 20
+        return secrionTitles.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -52,7 +76,6 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
             return UITableViewCell()
         }
-        cell.textLabel?.text = "hey"
         return cell
     }
     
@@ -62,6 +85,30 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 40
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        guard let header = view as? UITableViewHeaderFooterView else{
+            return
+        }
+        header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+        header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 20,
+                                         y: header.bounds.origin.y,
+                                         width: 100,
+                                         height: header.bounds.height)
+        header.textLabel?.textColor = .black
+        header.textLabel?.text = header.textLabel?.text?.lowercased()
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return secrionTitles[section]
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) { //navigation bar if scroll, stick to do top
+        let defaultOffset = view.safeAreaInsets.top
+        let offset = scrollView.contentOffset.y + defaultOffset
+        
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
     }
 }
 
